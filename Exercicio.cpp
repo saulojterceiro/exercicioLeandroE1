180d48776a1f6b6707efd4a86702b16949c12038
saulojterceiro <saulo.terceiro@ccc.ufcg.edu.br>
2019-03-26 21:19:27 -0300

    Primo funcionando

--- Exercicio.cpp
+++ Exercicio.cpp
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int Fatorial(){
+void Primo(){
+	int x;
+	printf("Escolha um numero para descobrir se é primo\n");
+	scanf("%d",&x);
+	bool primo = true;
+	if(x>1){
+		int i = 0;
+		for(i = 2;i<x;i++){
+			if(x%i==0){
+				primo = false;
+			}
+		}
+		if(primo){
+			printf("Primo \n");
+		}else{
+			printf("Nao primo \n");
+		}
+	}else{
+		printf("Numero invalido otr\n ");
+	}
+	
+
+	
+	
+}
+
+void Fatorial(){
 	int x;
 	printf("Escolha um numero para ser fatorizado\n");
 	scanf("%d",&x);
@@ -10,14 +36,18 @@ int Fatorial(){
 		x--;
 		somafatorial = somafatorial * (x);
 	}
-	return somafatorial;
-	printf("%d\n",fatorialN);
+	printf("%d\n",somafatorial);
 }
-void caseMenu(*int x){
+void caseMenu(int x){
 	
 	switch(x){
 		case(1):{
 			Fatorial();
+			break;
+		}
+		case(2):{
+			Primo();
+			break;
 		}
 	}
 	
@@ -28,7 +58,7 @@ int main(){
 	while(x!=0){
 		printf("Escolha uma opcao de 0 a 9 \n");
 		scanf("%d",&x);
-		caseMenu(&x);
+		caseMenu(x);
 	}
 	return 0;
 }
